9d2a01bf0953a817cf067b52789aa3c2eddad73c
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2018-01-09 11:39:36 -0500

    add new imports for test in test_authentication.py

--- hsm_emu/tests/context.py
+++ hsm_emu/tests/context.py
@@ -3,9 +3,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from libraries.utils_wallets import signMessage, verifyMessage, cipherKeyValue, decipherKeyValue
+from libraries.authentication import (getChallengeHidden, getChallengeVisual, 
+	checkPath, signAuth, verifyAuth)
+from libraries.utils_wallets import (signMessage, verifyMessage, cipherKeyValue, 
+	decipherKeyValue)
 from libraries.btcpy.structs.crypto import PublicKey, PrivateKey
 from libraries.btcpy.structs.script import P2pkhScript, P2shScript, P2wpkhV0Script
 from libraries.btcpy.structs.address import Address
 from libraries.btcpy.structs.hd import ExtendedKey, ExtendedPrivateKey, ExtendedPublicKey
 from libraries.btcpy.setup import setup
+from libraries.bitcoin.core import x, b2x
